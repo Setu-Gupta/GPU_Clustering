8c1a059509f35bd7e9eb0711d8f0fbe915ad111e
Setu-Gupta <setu18190@iiitd.ac.in>
2021-04-05 18:36:00 +0530

    Added various dataset examples for testing. Added scatter plot script.
    Author: setu18190@iiitd.ac.in
    
    On branch master
    Changes to be committed:
    	modified:   ../readme
    	new file:   datasets/Aggregation.txt
    	new file:   datasets/Compound.txt
    	new file:   datasets/D31.txt
    	new file:   datasets/R15.txt
    	new file:   datasets/flame.txt
    	new file:   datasets/jain.txt
    	new file:   datasets/pathbased.txt
    	new file:   datasets/spiral.txt
    	new file:   scatter.py

---
+++ test/scatter.py
@@ -0,0 +1,31 @@
+"""
+Creates a scatter plot for a 2D dataset
+The format of dataset is a list of the following entries
+x <space> y <space> cluster_number
+x and y are floating point values
+
+Usage:
+	python3 path/to/this/script.py path/to/dataset
+"""
+
+from matplotlib import pyplot as plt
+import sys
+
+data = {}
+
+for line in open(sys.argv[1], 'r'):
+	x, y, n = line.split()
+	x = float(x)
+	y = float(y)
+	
+	if n not in data:	# Create an entry if none exists
+		data[n] = [[], []]
+
+	data[n][0].append(x)
+	data[n][1].append(y)
+
+for n in data:
+	plt.scatter(data[n][0], data[n][1], label="Cluster " + str(n))
+
+plt.legend(loc="upper right")
+plt.show()
