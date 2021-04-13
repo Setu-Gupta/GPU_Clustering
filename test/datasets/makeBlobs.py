1d4ad9ad1d9d6a541fa61b1243809bb420100631
Setu-Gupta <setu18190@iiitd.ac.in>
2021-04-14 00:10:13 +0530

    Added more datsets and made makeBlobs more modular
    On branch master
    Changes to be committed:
    	new file:   BigData_100.txt
    	new file:   BigData_1000.txt
    	new file:   BigData_10000.txt
    	modified:   makeBlobs.py

--- test/datasets/makeBlobs.py
+++ test/datasets/makeBlobs.py
@@ -1,17 +1,29 @@
+"""
+Creates blob datasets of required number of datapoints and centers
+Usage: python3 path/to/this/file.py <n> <k> [<d>]
+where n is the number of datapoints, k is number of centers and d (default d=2) is number of dimentions
+"""
+
+
 from sklearn.datasets import make_blobs
-import matplotlib.pyplot as plt
-n = 300000
-X, y = make_blobs(n_samples=n, centers=500, n_features=2, random_state=1)
+import sys
+
+if(len(sys.argv) < 3):
+	print("Usage: python3 path/to/this/file.py <n> <k> [<d>]")
+	print("where n is the number of datapoints, k is number of centers and d (default d=2) is number of dimentions")
+
+n = int(sys.argv[1])
+k = int(sys.argv[2])
+d = 2
+if(len(sys.argv) == 4):
+	d = int(sys.argv[3])
+
+X, Y = make_blobs(n_samples=n, centers=k, n_features=d)
 
 F = open("BigData_"+str(n)+".txt", "w")
-F = open("BigData_"+str(n)+".txt", "a")
 for n in range(X.shape[0]):
-    s = str(X[n][0])+" "+str(X[n][1])+" "+str(y[n])+"\n"
-    F.writelines(s)
-
-
-# for n in range(X.shape[0]):
- #   plt.scatter(X[n][0], X[n][1])
-
-
-plt.show()
+	s = ""
+	for i in range(d):
+		s += str(X[n][i]) + " "
+	s += str(Y[n]) + "\n"
+	F.write(s)
